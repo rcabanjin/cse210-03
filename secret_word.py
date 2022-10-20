030f7b9b1a21425e5e3644768efeeea4c6b6d379
dic09005 <dic09005@byui.edu>
2022-10-19 18:54:40 -0600

    add file secret_word.py

---
+++ secret_word.py
@@ -0,0 +1,5 @@
+
+""" Secret_word (Rob)
+•	_init__ = Make a list of randoms words
+•	Get_hint = display message to inform the guesser
+•	Letter_found = Whether or not the guesser found the letter """
