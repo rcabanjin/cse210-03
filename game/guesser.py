76b62352bc4e5e8ecbc90685323dba9713202b8a
dic09005 <dic09005@byui.edu>
2022-10-20 20:56:29 -0600

    update file secret_word.py

--- game/guesser.py
+++ game/guesser.py
@@ -1,26 +1,24 @@
-from secret_word import random_word
+from secret_word import SecretWord
 
+random_word = SecretWord()
 word = random_word.word
 print(word)
+
+
 
 class guesser:
     def __init__(self, playerGuess):
         self.guess = playerGuess
 
-
-
-
-    def check_letter(self):
-        """Checks the letter against the word."""
-
-        for i in range(0,len(self.word)):
-            playerGuess = self.word[i]
-            if self.guess == playerGuess:
-                self.reveal[i] = self.guess
-        if '_' not in self.reveal:
-            return True
+    def checkLetter(self):
+        if not(playerGuess.isdigit()):
+            for character in word:
+                if(character == playerGuess):
+                    print("Correct!")
+                else:
+                    print("Try Again!")
         else:
-            return False
+            print("Please enter a letter, not a number")
 
 
 playerGuess = input("Enter a letter guess: ")
