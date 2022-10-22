bf057e12c2840120ea9f6c8096fd6ddae44d5105
dic09005 <dic09005@byui.edu>
2022-10-22 17:00:35 -0600

    add main file

---
+++ __main__.py
@@ -0,0 +1,12 @@
+
+from game.director import Director
+
+director = Director()
+print('        ____                                    ')                    
+print('       |    |__ __  _____  _____   ____  _____  ')  
+print('    ___|    |  |  |/     \|   _ \_/ __ \|  ___| ')
+print('   |   |    |  |  |  Y Y  |  |_| |  ___/|  |    ')
+print('   |________|_____|__|_|__|   __/ \____||__|    ')
+print('                          |__|                  ')
+
+director.start_game()
